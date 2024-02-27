f87d2b60421778f4f38d9c24019761b3333fbef0
arandomuserwouldbefine <custombana04e@gmail.com>
2024-02-27 22:30:30 +0530

    ref: removed window

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,6 @@ const font = Poppins({
 })
 
 export default async function Home() {
-  window.location.reload()
   await isAllowed()
   return (
     <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800 text-center">
