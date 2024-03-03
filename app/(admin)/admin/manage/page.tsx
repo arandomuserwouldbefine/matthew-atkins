d3c6023a2cfe51dd6d9b524c7f58b5b548647046
arandomuserwouldbefine <custombana04e@gmail.com>
2024-03-03 14:54:22 +0530

    changes

--- app/(admin)/admin/manage/page.tsx
+++ app/(admin)/admin/manage/page.tsx
@@ -7,18 +7,9 @@ const protocol = process.env.PROTOCOL
 
 
 
-async function getImages(){
-    const data = await fetch(`${protocol}://${url}/api/images`,{
-        method: "POST"
-    })
-    const result = data.json()
-    return result
-}
-
 
 
 export default async function ManageImages(){
-    const images = await getImages()
     return(
         <>
         <AdminNav/>
