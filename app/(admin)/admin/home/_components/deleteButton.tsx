1c714b36f51bb90ce252545f363ce1345074af22
arandomuserwouldbefine <custombana04e@gmail.com>
2024-03-04 15:51:37 +0530

    feat: delete image

--- app/(admin)/admin/home/_components/deleteButton.tsx
+++ app/(admin)/admin/home/_components/deleteButton.tsx
@@ -1,22 +1,32 @@
+"use client"
 import { Button } from "@/components/ui/button"
-import { logOut } from "@/actions/logout"
 import { startTransition } from "react"
 import { redirect } from "next/navigation"
 import Link from "next/link"
+import { useEdgeStore } from "@/lib/edgestore"
+import { PrismaClient } from "@prisma/client"
+import { db } from "@/lib/db"
 
-const onLogOut = ()=>{
-
-    startTransition(()=>{
-        logOut()
-        .then((data)=>{
-            console.log("Done logged out...")
-        })
-    })
+interface DeleteBtnProps {
+    id: string;
+    image_url: string;
 }
-export const DeleteBtn = async()=>{
-    return(
+
+export const DeleteBtn: React.FC<DeleteBtnProps> = ({ id, image_url }) => {
+    const {edgestore} = useEdgeStore()
+    return (
         <div>
-            <Button variant="destructive" size="sm" onClick={onLogOut}>Log Out</Button>           
+            <Button variant="destructive" size="sm" onClick={async()=>{
+                await edgestore.publicFiles.delete({
+                    url: image_url,
+                  });
+                await db.images.delete({
+                    where:{
+                        id:id
+                    }
+                })
+            }}>Delete</Button>
+           
         </div>
-    )
-}
+    );
+};
