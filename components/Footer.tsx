e3858b7bfa27ce4ef8564d24b67ab3ed961dc858
sohel <sohelshaikh01231@gmail.com>
2024-03-04 15:51:40 +0530

    Updated Home page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Footer() {
@@ -5,9 +6,8 @@ export default function Footer() {
     <section className="p-2 lg:px-[40px] lg:py-[50px] sm:p-3">
         <h1 className="kalnia text-[9.3vw]">MATTHEW ATKINS</h1>
         <ul className="text-lg lg:text-3xl font-semibold flex items-center justify-evenly">
-          <li><a href="/">HOME</a></li>
-          <li><a href="/">ABOUT</a></li>
-          <li><a href="/contact">CONTACT</a></li>
+          <li><Link href="/">ABOUT</Link></li>
+          <li><a href="contact-section">CONTACT</a></li>
         </ul>
       </section>
   )
