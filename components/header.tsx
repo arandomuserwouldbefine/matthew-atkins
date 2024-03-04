c6c7b389c2ae00bbc283bde3dc31972690e48b20
sohel <sohelshaikh01231@gmail.com>
2024-03-04 12:02:30 +0530

    Dark Mode Added

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,24 @@
-'use client'
+"use client";
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 import { FiSun } from "react-icons/fi";
+import { IoMoonSharp } from "react-icons/io5";
 
 export default function Header() {
   const pathname = usePathname();
-  console.log(pathname);
+  const [darkMode, setDarkMode] = useState(false);
+
+  const handleDarkModeChange = () => {
+    setDarkMode(!darkMode)
+    if(!darkMode){
+      document.querySelector("html")?.classList.add("dark")
+    }else{
+      document.querySelector("html")?.classList.remove("dark")
+    }
+  }
+
   return (
     <div className="border rounded-full px-8 sm:px-10 py-3">
       <ul className="flex items-center gap-12 text-lg lg:text-xl">
@@ -29,8 +41,8 @@ export default function Header() {
           </Link>
         </li>
         <li className="flex items-center">
-          <button>
-            <FiSun />
+          <button onClick={handleDarkModeChange}>
+            {!darkMode ? <FiSun /> : <IoMoonSharp />}
           </button>
         </li>
       </ul>
